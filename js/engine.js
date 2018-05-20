932be42275a66e4f441e2db8e6f9fa6068a7d80b
gunayanil <gunayanilemre@gmail.com>
2018-05-20 15:44:08 -0700

    added a draw function for the hearts

--- js/engine.js
+++ js/engine.js
@@ -4,7 +4,7 @@
  * render methods on your player and enemy objects (defined in your app.js).
  *
  * A game engine works by drawing the entire game screen over and over, kind of
- * like a flipbook you may have created as a kid. When your player moves across
+ * like a flip-book you may have created as a kid. When your player moves across
  * the screen, it may look like just that image/character is moving or being
  * drawn but that is not the case. What's really happening is the entire "scene"
  * is being drawn over and over, presenting the illusion of animation.
@@ -13,62 +13,60 @@
  * writing app.js a little simpler to work with.
  */
 
-var Engine = (function(global) {
+ var Engine = (function(global) {
     /* Predefine the variables we'll be using within this scope,
      * create the canvas element, grab the 2D context for that canvas
      * set the canvas elements height/width and add it to the DOM.
      */
-    var doc = global.document,
-        win = global.window,
-        canvas = doc.createElement('canvas'),
-        ctx = canvas.getContext('2d'),
-        lastTime;
-
-    canvas.width = 505;
-    canvas.height = 606;
-    doc.body.appendChild(canvas);
+     var doc = global.document,
+     win = global.window,
+     canvas = doc.createElement('canvas'),
+     ctx = canvas.getContext('2d'),
+     lastTime;
+     canvas.width = 505;
+     canvas.height = 606;
+     doc.body.appendChild(canvas);
 
     /* This function serves as the kickoff point for the game loop itself
      * and handles properly calling the update and render methods.
      */
-    function main() {
+     function main() {
         /* Get our time delta information which is required if your game
          * requires smooth animation. Because everyone's computer processes
          * instructions at different speeds we need a constant value that
          * would be the same for everyone (regardless of how fast their
          * computer is) - hurray time!
          */
-        var now = Date.now(),
-            dt = (now - lastTime) / 1000.0;
+         var now = Date.now(),
+         dt = (now - lastTime) / 1000.0;
 
         /* Call our update/render functions, pass along the time delta to
          * our update function since it may be used for smooth animation.
          */
-        update(dt);
-        render();
-        renderGem();
-        // updateGems();
-
+         update(dt);
+         render();
+       
         /* Set our lastTime variable which is used to determine the time delta
          * for the next time this function is called.
          */
-        lastTime = now;
+         lastTime = now;
 
         /* Use the browser's requestAnimationFrame function to call this
          * function again as soon as the browser is able to draw another frame.
          */
-        win.requestAnimationFrame(main);
-    }
+         win.requestAnimationFrame(main);
+     }
 
     /* This function does some initial setup that should only occur once,
      * particularly setting the lastTime variable that is required for the
      * game loop.
      */
-    function init() {
-        reset();
-        lastTime = Date.now();
-        main();
-    }
+     function init() {
+
+     	reset();
+     	lastTime = Date.now();
+     	main();
+     }
 
     /* This function is called by main (our game loop) and itself calls all
      * of the functions which may need to update entity's data. Based on how
@@ -79,36 +77,9 @@ var Engine = (function(global) {
      * functionality this way (you could just implement collision detection
      * on the entities themselves within your app.js file).
      */
-     function renderGem(){
-    const xCoor = [0, 100, 200, 300, 400];
-    const yCoor = [40, 130, 220];
-    let randomX = Math.floor(Math.random() * 5);
-    let randomY = Math.floor(Math.random() * 3);
-        var gemImages = [
-                'images/gem-blue.png',   // Top row is water
-                'images/gem-orange.png',   // Row 1 of 3 of stone
-                'images/gem-green.png'                
-                ];
-                for(var count=0; count < 3; count++){
-                    // 1. IT's definitely not about for - loop
-                    //xCoor[randomX] and yCoor[randomY] makes an infinity design on the scree
-                    // I mean gems are not located on the canvas, they always move on the canvas, don't stop!
-                    // No error message on the console.
-                  ctx.drawImage(Resources.get(gemImages[count]), xCoor[randomX], yCoor[randomY]);
 
-                  //ctx.drawImage(Resources.get(gemImages[count]), 100, 200); => this works!
-                  //Second problem: the size of the gem is too big? What do you suggest?
-
-                }
-
-          }
-
-    function updateGems(){
-        // gem.update();
-    }
-
-    function update(dt) {
-        updateEntities(dt);
+     function update(dt) {
+     	updateEntities(dt);
         // checkCollisions();
     }
 
@@ -119,14 +90,14 @@ var Engine = (function(global) {
      * the data/properties related to the object. Do your drawing in your
      * render methods.
      */
-    function updateEntities(dt) {
-        allEnemies.forEach(function(enemy) {
-            enemy.update(dt);
-        });
-        player.update();
-    }
+     function updateEntities(dt) {
+     	allEnemies.forEach(function(enemy) {
+     		enemy.update(dt);
+     	});
 
-    /*** MY ****/
+     	player.update();
+
+     }
 
 
     /* This function initially draws the "game level", it will then call
@@ -135,22 +106,27 @@ var Engine = (function(global) {
      * they are flipbooks creating the illusion of animation but in reality
      * they are just drawing the entire screen over and over.
      */
-    function render() {
+     function render() {
         /* This array holds the relative URL to the image used
          * for that particular row of the game level.
          */
-        var rowImages = [
+         // or(let put = 0; put < 3; put++) {
+          
+
+         
+
+         var rowImages = [
                 'images/water-block.png',   // Top row is water
                 'images/stone-block.png',   // Row 1 of 3 of stone
                 'images/stone-block.png',   // Row 2 of 3 of stone
                 'images/stone-block.png',   // Row 3 of 3 of stone
                 'images/grass-block.png',   // Row 1 of 2 of grass
                 'images/grass-block.png'    // Row 2 of 2 of grass
-            ],
-            numRows = 6,
-            numCols = 5,
-            row, col;
-        
+                ],
+                numRows = 6,
+                numCols = 5,
+                row, col;
+
         // Before drawing, clear existing canvas
         ctx.clearRect(0,0,canvas.width,canvas.height)
 
@@ -158,8 +134,8 @@ var Engine = (function(global) {
          * and, using the rowImages array, draw the correct image for that
          * portion of the "grid"
          */
-        for (row = 0; row < numRows; row++) {
-            for (col = 0; col < numCols; col++) {
+         for (row = 0; row < numRows; row++) {
+         	for (col = 0; col < numCols; col++) {
                 /* The drawImage function of the canvas' context element
                  * requires 3 parameters: the image to draw, the x coordinate
                  * to start drawing and the y coordinate to start drawing.
@@ -167,12 +143,15 @@ var Engine = (function(global) {
                  * so that we get the benefits of caching these images, since
                  * we're using them over and over.
                  */
-                ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
-            }
-        }
+                 ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
+             }
+         }
 
-        renderEntities();
-    }
+         ctx.drawImage(Resources.get('images/Heart.png'), 455, -15, 50, 80);
+            
+         renderEntities();
+         
+     }
 
 
 
@@ -181,45 +160,48 @@ var Engine = (function(global) {
      * tick. Its purpose is to then call the render functions you have defined
      * on your enemy and player entities within app.js
      */
-    function renderEntities() {
+     function renderEntities() {
         /* Loop through all of the objects within the allEnemies array and call
          * the render function you have defined.
          */
-        allEnemies.forEach(function(enemy) {
-            enemy.render();
-        });
+         allEnemies.forEach(function(enemy) {
+         	enemy.render();
+         });
 
-        player.render();
-    }
+         player.render();
+
+     }
 
     /* This function does nothing but it could have been a good place to
      * handle game reset states - maybe a new game menu or a game over screen
      * those sorts of things. It's only called once by the init() method.
      */
-    function reset() {
-        // noop
-    }
+     function reset() {
 
+     }
+
+   
     /* Go ahead and load all of the images we know we're going to need to
      * draw our game level. Then set init as the callback method, so that when
      * all of these images are properly loaded our game will start.
      */
-    Resources.load([
-        'images/stone-block.png',
-        'images/water-block.png',
-        'images/grass-block.png',
-        'images/enemy-bug.png',
-        'images/char-boy.png',
-        'images/char-horn-girl.png',
-        'images/gem-blue.png',
-        'images/gem-orange.png',
-        'images/gem-green.png'
-    ]);
-    Resources.onReady(init);
+     Resources.load([
+     	'images/stone-block.png',
+     	'images/water-block.png',
+     	'images/grass-block.png',
+     	'images/enemy-bug.png',
+     	'images/char-boy.png',
+     	'images/char-horn-girl.png',
+     	'images/gem-blue.png',
+     	'images/gem-orange.png',
+     	'images/gem-green.png',
+        'images/Heart.png'
+     	]);
+     Resources.onReady(init);
 
     /* Assign the canvas' context object to the global variable (the window
      * object when run in a browser) so that developers can use it more easily
      * from within their app.js files.
      */
-    global.ctx = ctx;
-})(this);
+     global.ctx = ctx;
+ })(this);
