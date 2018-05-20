d80d7e5ebd7784415721bf35f55e3e10bb346309
gunayanil <gunayanilemre@gmail.com>
2018-05-20 15:43:08 -0700

    added hearts, level and score

--- js/app.js
+++ js/app.js
@@ -1,20 +1,27 @@
+const score = document.getElementById('score');
+const getLevel = document.getElementById('level');
+let breakpoint = true;
+let point = 0;
+let level = 0;
+
+
 let Enemy = function(x, y, speed) {
     this.x = x; //horizontal
     this.y = y; // vertical
-    this.speed = Math.floor(Math.random() * 20) + 10;
+    this.speed = Math.floor(Math.random() * 110) + 55;
     this.sprite = 'images/enemy-bug.png';
- };
+};
 
- let enemy1 = new Enemy(-300, 40);
- let enemy2 = new Enemy(-300, 130);
- let enemy3 = new Enemy(-300, 220);
+let enemy1 = new Enemy(-300, 40);
+let enemy2 = new Enemy(-300, 130);
+let enemy3 = new Enemy(-300, 220);
 
 
- Enemy.prototype.update = function(dt) {
+Enemy.prototype.update = function(dt) {
  if(this.x <= 505) {  //canvas.width = 505
  	this.x = this.x + this.speed * dt;
  } else {
- 	this.x = -2;
+ 	this.x = -10;
  }
 };
 
@@ -24,43 +31,30 @@ Enemy.prototype.render = function() {
 };
 
 
-let Player = function(x, y) {
+let Player = function(x, y, hearts) {
 	this.sprite = 'images/char-horn-girl.png';
 	this.x = 200;
 	this.y = 400;
-}
-
-
-let Gem = function(x, y) {
-   this.x = x;
-   this.y = y;
-}
-
-Gem.prototype.render = function() {
-
-}
-
-Gem.prototype.update = function() {
-  if(gem.x === player.x){
-   if(player.y >= gem.y - 40 && player.y <= gem.y + 40) {
-      gem.x = -150;
-      gem.y = -150;
-   }
-}
+	this.hearts = 3;
 }
 
 Player.prototype.render = function(){
 	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+	ctx.font = '700 23pt Open Sans';
+	ctx.fillStyle = '#F94900';
+	ctx.fillText(`${player.hearts} x`, 410, 35);
+
 
 };
 
 Player.prototype.update = function(){
-
 	var player = this;
 	allEnemies.forEach(function(enemy) {
 		if(player.y === enemy.y) {
 			if(player.x >= enemy.x - 60 && player.x <= enemy.x + 60){
 				{
+					player.hearts -= 1;
+					ctx.fillText(`${player.hearts} x`, 410, 35);
 					player.x= 200;
 					player.y = 400;
 
@@ -69,19 +63,48 @@ Player.prototype.update = function(){
 		}
 	});
 
-	if(player.y < 40) { 
-		setTimeout(function() {
-			player.x = 200;
-			player.y = 400;
-		}, 800);
+	if(player.hearts === 0) {
+		point = 0;
+		score.textContent = point;
+		breakpoint = false;
+		player.reset();
+	}
 
+	if(breakpoint && player.y < 40) { // reaches the water
+		point += 100;
+		levelUpdate();
+		score.textContent = point;
+		player.x = 200;
+		player.y = 400;
 	}
 
 
 }
 
-Player.prototype.handleInput = function(key) {
+Player.prototype.reset = function() {
+	level = 0;
+	getLevel.textContent = level;
+	breakpoint= true;
+	player.hearts = 3;
+	player.x = 200;
+	player.y = 400;
+	allEnemies.forEach(function(enemy){
+	enemy.speed = Math.floor(Math.random() * 110) + 55;
 
+	})
+
+}
+
+function levelUpdate() {
+	level++
+	allEnemies.forEach(function(enemy){
+		enemy.speed *= 1.25;
+	})
+	getLevel.textContent = level;
+
+}
+
+Player.prototype.handleInput = function(key) {
 	if(key === "up" && this.y > 0){
 		this.y -= 90;
 	}
@@ -101,8 +124,7 @@ Player.prototype.handleInput = function(key) {
 
 
 let allEnemies = [enemy1, enemy2, enemy3];
-let player = new Player(50, 10);
-let gem = new Gem(200, 130);
+let player = new Player();
 
 document.addEventListener('keyup', function(e) {
 	var allowedKeys = {
